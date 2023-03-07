fe1a09c1afb4b6197dd014c5ac342442a7541577
Paula Sousa <paulasousa137@gmail.com>
2023-03-07 12:20:41 +0100

    Imprimir variables

---
+++ src/Ej3.java
@@ -0,0 +1,12 @@
+
+public class Ej3 {
+	public static void main(String args[]) {
+		int x=1;
+		int y=2;
+		double n=1.5;
+		double m=2.5;
+		
+		System.out.println("Las variables creadas son: x = "+x+ ", "+ "y = "+y+", "+"n = "+n+" y "+"m = "+m+".");
+	}
+
+}
