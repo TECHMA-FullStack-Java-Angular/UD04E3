261ce2863d7609a87eaf77f1fe9a69218da9384b
Paula Sousa <paulasousa137@gmail.com>
2023-03-07 12:35:52 +0100

    final

--- src/Ej3.java
+++ src/Ej3.java
@@ -17,6 +17,13 @@ public class Ej3 {
 		System.out.println("n * m = "+(n*m));
 		System.out.println("n / m = "+(n/m));
 		System.out.println("n % m = "+(n%m));
+		System.out.println("x + n = "+(x+n));
+		System.out.println("y / m = "+(y/m));
+		System.out.println("y % m = "+(y%m));
+		System.out.println("El doble de las variables x, y, n y m es "+(x*2)+", "+ (y*2)+", "+ (n*2)+ " y "+(m*2)+" respectivamente.");
+		System.out.println("La suma de todas las variables es "+(x+y+n+m));
+		System.out.println("El producto de todas las variables es "+(x*y*n*m));
+
 	}
 
 }
